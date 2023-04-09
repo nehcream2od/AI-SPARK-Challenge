8c4d214b773115358a9e8d420769e108ee0c0649
nehcream <nehcream@gmail.com>
2023-04-09 20:18:54 +0900

    refactoring trainer

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -22,23 +22,13 @@ class LitGANTrainer(BaseLitModule):
         self.disc_optimizer_class = disc_optimizer_class
         self.config = config
         self.alpha = alpha
-        self.automatic_optimization = False
 
     def forward(self, x):
         return self.generator(x)
 
-    def training_step(self, batch, batch_idx):
-        inputs = batch
-        batch_size = inputs.shape[0]
-
-        optimizer_g, optimizer_d = self.optimizers()
-
-        # Train generator
-        self.toggle_optimizer(optimizer_g, 0)
-        generated_inputs = self.generator(inputs)
-        valid = torch.ones(batch_size, 1, device=self.device)
-
-        g_loss = self.criterion_gen(
+    def _generator_loss(self, generated_inputs, inputs):
+        valid = torch.ones(inputs.shape[0], 1, device=self.device)
+        return self.criterion_gen(
             generated_inputs, inputs
         ) * self.alpha + self.criterion_disc(
             self.discriminator(generated_inputs), valid
@@ -46,55 +36,49 @@ class LitGANTrainer(BaseLitModule):
             1 - self.alpha
         )
 
-        self.log(
-            "train_generator_loss", g_loss, on_step=True, on_epoch=True, prog_bar=True
-        )
-        self.manual_backward(g_loss)
-        optimizer_g.step()
-        optimizer_g.zero_grad()
-        self.untoggle_optimizer(optimizer_g)
+    def _discriminator_loss(self, real_inputs, fake_inputs):
+        valid = torch.ones(real_inputs.shape[0], 1, device=self.device)
+        fake = torch.zeros(real_inputs.shape[0], 1, device=self.device)
+        return self.criterion_disc(
+            self.discriminator(real_inputs), valid
+        ) + self.criterion_disc(self.discriminator(fake_inputs.detach()), fake)
 
-        # Train discriminator
-        self.toggle_optimizer(optimizer_d, 1)
-        valid = torch.ones(batch_size, 1, device=self.device)
-        real_loss = self.criterion_disc(self.discriminator(inputs), valid)
-        fake = torch.zeros(batch_size, 1, device=self.device)
-        fake_loss = self.criterion_disc(
-            self.discriminator(generated_inputs.detach()), fake
-        )
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        inputs = batch
 
-        d_loss = (real_loss + fake_loss) / 2
-        self.log(
-            "train_discriminator_loss",
-            d_loss,
-            on_step=True,
-            on_epoch=True,
-            prog_bar=True,
-        )
-        self.manual_backward(d_loss)
-        optimizer_d.step()
-        optimizer_d.zero_grad()
-        self.untoggle_optimizer(optimizer_d)
+        if optimizer_idx == 0:
+            generated_inputs = self.generator(inputs)
+            g_loss = self._generator_loss(generated_inputs, inputs)
+            self.log(
+                "train_generator_loss",
+                g_loss,
+                on_step=True,
+                on_epoch=True,
+                prog_bar=True,
+            )
+            return g_loss
+
+        elif optimizer_idx == 1:
+            generated_inputs = self.generator(inputs)
+            d_loss = self._discriminator_loss(inputs, generated_inputs)
+            self.log(
+                "train_discriminator_loss",
+                d_loss,
+                on_step=True,
+                on_epoch=True,
+                prog_bar=True,
+            )
+            return d_loss
 
     def validation_step(self, batch, batch_idx):
         inputs = batch
         batch_size = inputs.size(0)
 
         with torch.no_grad():
-            real_output = self.discriminator(inputs)
-            valid = torch.ones(batch_size, 1, device=self.device)
-            real_loss = self.criterion_disc(real_output, valid)
-
             generated_inputs = self.generator(inputs)
-            fake_output = self.discriminator(generated_inputs)
-            fake = torch.zeros(batch_size, 1, device=self.device)
-            fake_loss = self.criterion_disc(fake_output, fake)
+            disc_loss = self._discriminator_loss(inputs, generated_inputs)
 
-            disc_loss = real_loss + fake_loss
-
-            gen_loss = self.criterion_gen(
-                generated_inputs, inputs
-            ) * self.alpha + self.criterion_disc(fake_output, valid) * (1 - self.alpha)
+            gen_loss = self._generator_loss(generated_inputs, inputs)
 
         return {"val_discriminator_loss": disc_loss, "val_generator_loss": gen_loss}
 
